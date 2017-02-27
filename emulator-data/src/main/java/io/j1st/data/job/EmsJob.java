b4d7363d7a8aa674563db2f3941a057edce38b4b
xiaopeng <18971903450@163.com>
2017-02-27 09:32:42 +0800

    V1.0暂无BUG版,优化随机算法

--- emulator-data/src/main/java/io/j1st/data/job/EmsJob.java
+++ emulator-data/src/main/java/io/j1st/data/job/EmsJob.java
@@ -180,12 +180,11 @@ public class EmsJob extends Thread {
         cCloud[0] = ran > 5 ? 1 : ran > 3 ? 2 : ran > 2 ? 3 : 4;
         cCloud[1] = ran > 5 ? 3 : ran > 3 ? 2 : ran > 2 ? 1 : 5;
         cCloud[2] = ran > 5 ? 0 : ran > 3 ? 1 : ran > 2 ? 2 : 3;
-        ran = (int) (Math.random() * 10);
         cCloud[3] = ran > 5 ? 0 : ran > 3 ? 1 : ran > 2 ? 3 : 2;
         cCloud[4] = ran > 5 ? 0 : ran > 3 ? 1 : ran > 2 ? 2 : 3;
         cCloud[5] = ran > 5 ? 6 : ran > 3 ? 5 : ran > 2 ? 4 : 7;
-        ran = (int) (Math.random() * 10);
         cCloud[6] = ran > 5 ? 6 : ran > 3 ? 5 : ran > 2 ? 4 : 3;
+        ran = (int) (Math.random() * 10);
         cCloud[7] = ran > 5 ? 3 : ran > 3 ? 4 : ran > 2 ? 1 : 2;
         return cCloud;
     }
